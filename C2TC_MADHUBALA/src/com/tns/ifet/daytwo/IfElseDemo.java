e389831eb76566ec19294a01198f8d00a464e82c
HP <HP@localhost>
2024-11-15 15:03:05 +0530

    initial commit

---
+++ C2TC_MADHUBALA/src/com/tns/ifet/daytwo/IfElseDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.daytwo;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=11;
+		if(age>=18)
+		System.out.println(" You are eligiblefor Voting");
+		else
+		System.out.println("Sorry! You need towait more");
+		System.out.println("if else demonstration");
+	}
+
+}
